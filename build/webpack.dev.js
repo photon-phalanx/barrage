03d25a718648a95d88e2c83ed876b11ad0b6ac22
wangjiachen <wangjc.sp@fuson.com>
2017-12-12 11:50:01 +0800

    first-commit

---
+++ build/webpack.dev.js
@@ -0,0 +1,48 @@
+var path = require('path')
+var webpack = require('webpack')
+
+function resolve(dir) {
+  return path.join(__dirname, '..', dir)
+}
+
+module.exports = {
+  entry: './src/index',
+  output: {
+    path: resolve('dist'),
+    filename: 'barrage.js',
+    publicPath: '/assets/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'eslint-loader',
+        enforce: "pre",
+        include: [resolve('src')],
+        options: {
+          formatter: require('eslint-friendly-formatter')
+        }
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        include: [resolve('src')]
+      }
+    ]
+  },
+  devServer: {
+    historyApiFallback: true,
+    // noInfo: true,
+    noInfo: false,
+    overlay: true,
+    port: 3000,
+    proxy: {
+      '/api/*': {
+        target: 'http://127.0.0.1:8000',
+        changeOrigin: true,
+        secure:false
+      }
+    }
+  },
+  devtool: '#eval-source-map'
+};
